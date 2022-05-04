23cae1119c27bc56823c7d277adf5b38b204debf
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2022-05-04 13:46:15 -0600

    created let block in calculator_spec.rb

--- RSpecCalculator/spec/calculator_spec.rb
+++ RSpecCalculator/spec/calculator_spec.rb
@@ -1,16 +1,18 @@
 require './lib/calculator'
+require 'spec_helper'
 
 describe Calculator do
+  before :all do
+    @calculator = Calculator.new
+  end
   describe "#add" do
     it "returns the sum of more than two numbers" do
-      calculator = Calculator.new
-      expect(calculator.add(2, 5, 7)).to eql(14)
+      expect(@calculator.add(2, 5, 7)).to eq(14)
     end
   end
   describe "#multiply" do
     it "returns the sum of two numbers multiplied by one another" do
-      calculator = Calculator.new
-      expect(calculator.multiply(5, 10)).to eql(50)
+      expect(@calculator.multiply(5, 10)).to eql(50)
     end
   end
 end
